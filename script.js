f181acd9253373bb0a95606e310f3764affe43c4
Programmerbikash <bikash19081998dey@gmail.com>
2022-09-14 23:07:02 +0600

    typing start finished

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -8,6 +8,7 @@ const modalBackground = document.getElementById("modal-background");
 // variables
 let userText = "";
 let errorCount = 0;
+// let errorCount = '';
 let startTime;
 let questionText = "";
 
@@ -90,6 +91,7 @@ const gameOver = () => {
   // restart everything
   startTime = null;
   errorCount = 0;
+  // errorCount++;
   userText = "";
   display.classList.add("inactive");
 };
@@ -107,18 +109,20 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      // countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
-
+      // console.log(count);
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
+    // console.log('out side count')
     count--;
   }, 1000);
 };
@@ -133,7 +137,9 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
-
+  
+  // console.log(startTime)
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  // console.log(startTime)
 }, 1000);
